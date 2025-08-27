03389c5d3fd01f638478f59eeaab1311095c63d5
rokudara-sen <160833839+rokudara-sen@users.noreply.github.com>
2025-08-27 08:17:31 +0200

    Reuse mutineer icon for leader

---
+++ Content.Client/_RMC14/Marines/Mutiny/MutineerInviteWindow.cs
@@ -0,0 +1,58 @@
+using System.Numerics;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Shared.Localization;
+using static Robust.Client.UserInterface.Controls.BoxContainer;
+
+namespace Content.Client._RMC14.Marines.Mutiny;
+
+public sealed class MutineerInviteWindow : DefaultWindow
+{
+    public readonly Button DenyButton;
+    public readonly Button AcceptButton;
+
+    public MutineerInviteWindow()
+    {
+        Title = Loc.GetString("mutineer-invite-title");
+
+        Contents.AddChild(new BoxContainer
+        {
+            Orientation = LayoutOrientation.Vertical,
+            Children =
+            {
+                new BoxContainer
+                {
+                    Orientation = LayoutOrientation.Vertical,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = Loc.GetString("mutineer-invite-text")
+                        },
+                        new BoxContainer
+                        {
+                            Orientation = LayoutOrientation.Horizontal,
+                            Align = AlignMode.Center,
+                            Children =
+                            {
+                                AcceptButton = new Button
+                                {
+                                    Text = Loc.GetString("mutineer-invite-accept")
+                                },
+                                new Control
+                                {
+                                    MinSize = new Vector2(20, 0)
+                                },
+                                DenyButton = new Button
+                                {
+                                    Text = Loc.GetString("mutineer-invite-deny")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
